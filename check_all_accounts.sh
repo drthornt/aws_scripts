89f54a18514a019bf481c29a7bd240fa9c1dae63
David Thornton <northdot9@gmail.com>
2017-10-10 12:49:20 -0400

    sooper dooper, data troooper\!

--- check_all_accounts.sh
+++ check_all_accounts.sh
@@ -12,9 +12,16 @@ echo -n $i
 #ls -la ~/.password-store/${i}/access-key.gpg
 if [ -e ~/.password-store/${i}/access-key.gpg -a -e ~/.password-store/${i}/secret-key.gpg ]
 then
-  echo " is aws"
-else
-  echo " is NOT aws"
-  ls -la ~/.password-store/${i}/
+  # echo " is aws"
+  export TF_VAR_aws_access_key="$(pass ${i}/access-key)"
+  export i=${i}
+  export AWS_ACCESS_KEY_ID="$(pass ${i}/access-key)"
+  export AWS_SECRET_ACCESS_KEY="$(pass ${i}/secret-key)"
+  echo -n " "
+  ./root_mfa.py
+  
+#else
+  # echo " is NOT aws"
+  # ls -la ~/.password-store/${i}/
 fi
 done
--- root_mfa.py
+++ root_mfa.py
@@ -1,20 +1,27 @@
-#!/usr/bin/rnv python
-#refrence: https://stackoverflow.com/questions/29339566/how-to-check-whether-mfa-is-enabled-for-root-account-in-aws-using-boto
+#!/usr/bin/env python
+# reference: https://stackoverflow.com/questions/29339566/how-to-check-whether-mfa-is-enabled-for-root-account-in-aws-using-boto
 
 import boto.iam
+import boto3
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
 conn = boto.iam.connect_to_region('us-east-1')
+
+client = boto3.client('iam')
+
 summary = conn.get_account_summary()
 
-print "SUMMARY"
-pp.pprint(summary)
+acc_alias = client.list_account_aliases()
 
-# This returns a Python dictionary containing a lot of information about your account. Specifically, to find out if MFA is enabled;
+listofaliases = acc_alias['AccountAliases'] 
+
+for alias in  listofaliases:
+ print alias
 
 if summary['AccountMFAEnabled']:
     print "MFA is enabled"
 else:
     print "MFA is not enabled"
+
