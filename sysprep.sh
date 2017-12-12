4dd577835674cc415c3ad038623fe49fbc744281
David Thornton <northdot9@gmail.com>
2017-12-11 22:14:57 -0500

    cleaned up sysprep a bit, added history -c

--- sysprep.sh
+++ sysprep.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
-
 # do this to an aws instance before you down it to become an ami.
 
-shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
-
-rm -rf /home/*/.*history
-rm -rf /root/.bash_history
-
-rm -rf /var/log/messages
-rm -rf /var/log/lastlog
+/bin/shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
+/bin/shred -u /home/*/.*history
+/bin/shred -u /root/.bash_history
+for i in messages wtmp btmp secure cron dmesg dmesg.old
+do
+/bin/shred -u /var/log/${i}
+done
 yum clean all
 rm -rf /var/cache/yum
-rm -rf /etc/sethostname
 rm -rf /etc/hostname
-
-# It can be temping to remove /etc/machine-id but this can be problematic as 
-# the AWS provided RHEL instance doesn't have "--setup-machine-id --root=/" 
-# in it's firstboot service, which means that /etc/machine-id _never_ gets 
-# created. This is bad and among other things, journald will fail.
-
+history -c
