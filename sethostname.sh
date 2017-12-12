3a28737a78eb5fc03e0ddf994a548b4d4453299f
David Thornton <northdot9@gmail.com>
2017-12-11 19:41:58 -0500

    updated to sethostname and sysprep.sh

--- sethostname.sh
+++ sethostname.sh
@@ -3,30 +3,28 @@
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-hostname.html
 # https://www.freedesktop.org/software/systemd/man/hostnamectl.html
 
-# on redhal7 systems add this to rc.local
-# /etc/rc.d/rc.local                
-# does this also work on ubuntu?
+# tested on rhel7
+# for aws ec2 instances that don't get their hostname set from user-data
 
 if [ -x /etc/sethostname ]; then
   echo found /etc/sethostname exiting
   exit
 fi
 
-echo setting hostname in sethostname.sh
-SLEEP=10
-echo sleep $SLEEP
-# sleep $SLEEP
+if [ -e /etc/profile.d/proxy.sh ]; then
+        source /etc/profile.d/proxy.sh
+fi
 
 # if you are using a proxy then ensure that  you have set NO_PROXY=169.254.169.254 ( link local meta data service ip )
 export NO_PROXY=169.254.169.254
 
-# domain is set by dhcp
 export DOMAIN=`dnsdomainname`
 echo domain is ${DOMAIN}
-
-echo raw curl
-/bin/curl -v http://169.254.169.254/latest/meta-data/placement/availability-zone
-echo
+if [ ${#SERVER} -lt 1 ] ; then
+        echo domain is not set, use resolv.conf, ugly
+        DOMAIN=`grep search /etc/resolv.conf | cut -d ' ' -f 2`
+        echo got domain ${DOMAIN}
+fi
 
 export EC2_AVAIL_ZONE=`/bin/curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 echo EC2_AVAIL_ZONE ${EC2_AVAIL_ZONE}
@@ -39,49 +37,50 @@ fi
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 echo EC2_REGION ${EC2_REGION}
 
-echo aws call
-echo /usr/bin/aws ec2 describe-tags --region ${EC2_REGION} --filters "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" "Name=key,Values=Name" --query 'Tags[*].Value' --output text
-/usr/bin/aws ec2 describe-tags --region ${EC2_REGION} --filters "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" "Name=key,Values=Name" --query 'Tags[*].Value' --output text
-echo done aws call
-
 INSTANCEID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 echo INSTANCEID is ${INSTANCEID}
 
-SERVER=`/usr/bin/aws ec2 describe-tags --region ${EC2_REGION} --filters "Name=resource-id,Values=${INSTANCEID}" "Name=key,Values=Name" --query 'Tags[*].Value' --output text`
 PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+echo PRIVATE_IP $PRIVATE_IP
 
-if [ ${#SERVER} -lt 1 ]; then
-        echo unable to get server name from Tag, exiting
-        exit
-fi
+SERVER=""
+i=0
 
-echo "$PRIVATE_IP $SERVER" >> /etc/hosts
-
-grep -v ${PRIVATE_IP} /etc/hosts > /tmp/tmp.host
-echo "$PRIVATE_IP $SERVER" >> /etc/hosts
-cp -f /tmp/tmp.host /etc/hosts
+while [ \( ${#SERVER} -lt 1 \) ]; do
+        i=$[$i+1]
+        echo getting server name $i
+        /usr/bin/aws ec2 describe-tags --region ${EC2_REGION} --filters "Name=resource-id,Values=${INSTANCEID}" "Name=key,Values=Name" --query 'Tags[*].Value' --output text 2>&1 > /var/log/sethostname.aws
+        sleep 1
+        TMP=`/usr/bin/aws ec2 describe-tags --region ${EC2_REGION} --filters "Name=resource-id,Values=${INSTANCEID}" "Name=key,Values=Name" --query 'Tags[*].Value' --output text`
+        echo TMP is $TMP
+        SERVER=${TMP// /-}
+        echo SERVER is ${SERVER}
+        sleep 1
+done
 
 sed -i "s/ localhost / $SERVER /" /etc/hosts
 
-hostname ${SERVER}.${DOMAIN}
+echo ${SERVER}.${DOMAIN} > /etc/hostname
 
 if [ -x /bin/hostnamectl ]; then
-        echo /bin/hostnamectl before
-        /bin/hostnamectl
-        /bin/hostnamectl set-hostname ${SERVER}.${DOMAIN}
-        echo /bin/hostnamectl after
-        /bin/hostnamectl
+        echo hostnamectl before
+        time /bin/hostnamectl 2>&1 > /var/log/sethostname.hostnamectl.before
+        echo /bin/hostnamectl set-hostname ${SERVER}.${DOMAIN}
+        time /bin/hostnamectl set-hostname ${SERVER}.${DOMAIN}
+        echo hostnamectl after
+        time /bin/hostnamectl 2>&1 > /var/log/sethostname.hostnamectl.after
 fi
 
 if [ -e /etc/sysconfig/network ] ; then
-        grep HOSTNAME /etc/sysconfig/network 1>&2 2>/dev/null
+        # grep HOSTNAME /etc/sysconfig/network 1>&2 2>/dev/null
+        grep HOSTNAME /etc/sysconfig/network
         ERR=$?
         if [ ${ERR} == "0" ] ; then
                 echo HOSTNAME is in /etc/sysconfig/network then use sed to edit
                 sed -i "s/^\(HOSTNAME\s*=\s*\).*$/\1$SERVER/" /etc/sysconfig/network
         else
                 echo HOSTNAME is not in /etc/sysconfig/network, add.
-                echo "HOSTNAME=$SERVER" # >> /etc/sysconfig/network
+                echo "HOSTNAME=$SERVER" >> /etc/sysconfig/network
         fi
 fi
 
--- sysprep.sh
+++ sysprep.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+# do this to an aws instance before you down it to become an ami.
+
 shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 
 rm -rf /home/*/.*history
 rm -rf /root/.bash_history
 
-#rm -rf /var/log/messages
-#rm -rf /var/log/lastlog
+rm -rf /var/log/messages
+rm -rf /var/log/lastlog
 yum clean all
 rm -rf /var/cache/yum
 rm -rf /etc/sethostname
-rm -rf /etc/machine-id
+rm -rf /etc/hostname
+
+# It can be atemping to remove /etc/machine-id but this can be problematic as 
+# the AWS provided RHEL instance doesn't have "--setup-machine-id --root=/" 
+# in it's firstboot service, which means that /etc/machine-id _never_ gets 
+# created. This is bad and among other things, journald will fail.
+
